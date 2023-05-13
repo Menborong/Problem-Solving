41614610fe589e108c2595ee578a05b4f26d3d7e
menborong <banana7@naver.com>
2023-05-13 19:36:51 +0900

    update template

--- templates/basic_tempate.cpp
+++ templates/basic_tempate.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using vInt = vector<int>;
 using LL = long long;
+#define int LL // check!!!!!!!
+
+
+using vInt = vector<int>;
 using vLL = vector<LL>;
 using pLL = pair<LL, LL>;
 using vPLL = vector<pLL>;
@@ -15,8 +18,8 @@ using matBool = vector<vBool>;
 using vStr = vector<string>;
 
 #define all(x) (x).begin(), (x).end()
-#define cyes cout << "Yes" << '\n'
-#define cno cout << "No" << '\n'
+#define cyes cout << "YES" << '\n'
+#define cno cout << "NO" << '\n'
 #define for0(i, n) for(int (i)=0; (i)<(n); (i)++)
 #define for1(i, n) for(int (i)=1; (i)<=(n); (i)++) 
 #define newInt(n) int n; cin >> n
@@ -27,7 +30,10 @@ using vStr = vector<string>;
 #define ff first
 #define ss second
 
-const int MOD = 998244353; 
+/////////////////////////////////////////////////////////
+
+const int MOD = 998244353;
+
 int modAdd(LL a, LL b){
     return (a+b)%MOD;
 }
@@ -63,15 +69,181 @@ int getC(int N, int M){
     return ans;
 }
 
+/////////////////////////////////////////////////////////
+
+struct SumSeg{
+    int N;
+    vInt seg;
+    SumSeg(int N): N(N){
+        int siz = 1;
+        while(siz < 2*N) siz <<= 1;
+        seg = vInt(siz+1);
+    }
+    int merge(int a, int b){
+        // can be changed
+        return a+b;
+    }
+    void init(int idx, int s, int e, vInt& A){
+        if(s == e){
+            seg[idx] = A[s];
+            return;
+        }
+        int m = s+e >> 1;
+        init(idx<<1, s, m, A);
+        init(idx<<1|1, m+1, e, A);
+        seg[idx] = merge(seg[idx<<1], seg[idx<<1|1]);
+    }
+    void update(int tar, int v, int idx, int s, int e){
+        if(tar > e || tar < s) return;
+        if(s == e){
+            seg[idx] = v; // can be changed
+            return;
+        }
+        int m = s+e >> 1;
+        update(tar, v, idx<<1, s, m);
+        update(tar, v, idx<<1|1, m+1, e);
+        seg[idx] = merge(seg[idx<<1], seg[idx<<1|1]);
+    }
+    int query(int l, int r, int idx, int s, int e){
+        if(r < s || e < l) return 0; // can be changed
+        if(l <= s && e <= r) return seg[idx];
+        int m = s+e >> 1;
+        return merge(query(l, r, idx<<1, s, m), query(l, r, idx<<1|1, m+1, e));
+    }
+};
+
+/////////////////////////////////////////////////////////
+
+struct RMQSeg{
+    int N;
+    vInt seg;
+    RMQSeg(int N): N(N){
+        int siz = 1;
+        while(siz < 2*N) siz <<= 1;
+        seg = vInt(siz+1, -1e18);
+    }
+    int merge(int a, int b){
+        // can be changed
+        return max(a, b);
+    }
+    void init(int idx, int s, int e, vInt& A){
+        if(s == e){
+            seg[idx] = A[s];
+            return;
+        }
+        int m = s+e >> 1;
+        init(idx<<1, s, m, A);
+        init(idx<<1|1, m+1, e, A);
+        seg[idx] = merge(seg[idx<<1], seg[idx<<1|1]);
+    }
+    void update(int tar, int v, int idx, int s, int e){
+        if(tar > e || tar < s) return;
+        if(s == e){
+            seg[idx] = v; // can be changed
+            return;
+        }
+        int m = s+e >> 1;
+        update(tar, v, idx<<1, s, m);
+        update(tar, v, idx<<1|1, m+1, e);
+        seg[idx] = merge(seg[idx<<1], seg[idx<<1|1]);
+    }
+    int query(int l, int r, int idx, int s, int e){
+        if(r < s || e < l) return -1e18; // can be changed
+        if(l <= s && e <= r) return seg[idx];
+        int m = s+e >> 1;
+        return merge(query(l, r, idx<<1, s, m), query(l, r, idx<<1|1, m+1, e));
+    }
+};
+
+/////////////////////////////////////////////////////////
+
+struct RMQIdxSeg{
+    int N;
+    vInt seg;
+    vInt& A;
+    RMQIdxSeg(int N, vInt& A): N(N), A(A){
+        int siz = 1;
+        while(siz < 2*N) siz <<= 1;
+        seg = vInt(siz+1, -1);
+    }
+    int merge(int a, int b){
+        // can be changed
+        if(a == -1) return b;
+        if(b == -1) return a;
+        return A[a] < A[b] ? a : b;
+    }
+    void init(int idx, int s, int e, vInt& A){
+        if(s == e){
+            seg[idx] = s;
+            return;
+        }
+        int m = s+e >> 1;
+        init(idx<<1, s, m, A);
+        init(idx<<1|1, m+1, e, A);
+        seg[idx] = merge(seg[idx<<1], seg[idx<<1|1]);
+    }
+    void update(int tar, int v, int idx, int s, int e){
+        if(tar > e || tar < s) return;
+        if(s == e){
+            seg[idx] = v; // can be changed
+            return;
+        }
+        int m = s+e >> 1;
+        update(tar, v, idx<<1, s, m);
+        update(tar, v, idx<<1|1, m+1, e);
+        seg[idx] = merge(seg[idx<<1], seg[idx<<1|1]);
+    }
+    int query(int l, int r, int idx, int s, int e){
+        if(r < s || e < l) return -1; // can be changed
+        if(l <= s && e <= r) return seg[idx];
+        int m = s+e >> 1;
+        return merge(query(l, r, idx<<1, s, m), query(l, r, idx<<1|1, m+1, e));
+    }
+};
+
+/////////////////////////////////////////////////////////
+
+
+
+struct Disj{
+    vInt disj;
+    vInt depth;
+    Disj(int N=1){
+        disj = vInt(N+1);
+        depth = vInt(N+1);
+        for(int i=1; i<=N; i++)
+            disj[i] = i;
+    }
+    int disjF(int u){
+        while(u != disj[u])
+            u = disj[u];
+        return u;
+    }
+    void disjU(int u, int v){
+        u = disjF(u);
+        v = disjF(v);
+        if(u == v) return;
+        if(depth[u] < depth[v])
+            swap(u, v);
+        if(depth[u] == depth[v])
+            depth[u]++;
+        disj[v] = u; 
+    }
+};
+
+
 void solve(){
     
 }
 
-int main(){
+int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    solve();
+    int T;
+    cin >> T;
+    while(T--)
+        solve();
 
     return 0;
 }
